67f7591c7fbdfeac7b7a42da2445289f1de38269
niklasbaier <niklas.baier@yahoo.de>
2024-06-01 13:52:14 +0200

    feat: add docker-compose.yaml for easy app setup

--- scripts/cicd/setup/read_poetry_version.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux
-
-POETRY_VERSION=$(cat .poetry-version)
-echo "$POETRY_VERSION"
--- scripts/cicd/setup/read_python_version.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux
-
-PYTHON_VERSION=$(cat .python-version)
-echo "$PYTHON_VERSION"
